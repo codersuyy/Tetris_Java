967383bc97bf6957baf578e8504d0898d1989815
codersuyy <vuong211.work@gmail.com>
2024-04-09 00:29:23 +0700

    Fix home panel

--- Tetris/src/Design/Content.java
+++ Tetris/src/Design/Content.java
@@ -1,5 +1,5 @@
 package Design;
 
 public class Content {
-
+	
 }
---
+++ Tetris/src/Design/Play.java
@@ -0,0 +1,14 @@
+package Design;
+
+import java.awt.Color;
+
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class Play extends JPanel{
+	public Play(){
+		setBackground(Color.BLACK);
+		setBorder(new EmptyBorder(5, 5, 5, 5));
+		setLayout(null);
+	}
+}
