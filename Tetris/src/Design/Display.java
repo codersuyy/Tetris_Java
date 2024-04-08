710bcbcab779320484b8ecb7746a3d98049f5d66
Loz <tuanchu24112004@gmail.com>
2024-04-08 20:34:21 +0700

    home 1

---
+++ Tetris/src/Design/Display.java
@@ -0,0 +1,20 @@
+package Design;
+
+import java.awt.EventQueue;
+
+public class Display {
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Home frame = new Home();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+
+		
+	}
+}
