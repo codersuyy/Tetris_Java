7485133697674c071481bbc852279b6b36f35560
codersuyy <vuong211.work@gmail.com>
2024-04-09 00:49:24 +0700

    xoa bot phan thua

--- Tetris/src/Design/Home.java
+++ Tetris/src/Design/Home.java
@@ -1,17 +1,10 @@
 package Design;
 
-import java.awt.BorderLayout;
-import java.awt.EventQueue;
-
-import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
 import java.awt.Font;
 import java.awt.Color;
-import javax.swing.JButton;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
 import javax.swing.SwingConstants;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
