2134de12f5b68e965c0abb8cefcd52da0abe29ca
codersuyy <vuong211.work@gmail.com>
2024-04-08 16:27:32 +0700

    sua giao dien

--- Tetris/src/Design/Home.java
+++ Tetris/src/Design/Home.java
@@ -8,11 +8,13 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
 import java.awt.Font;
+import java.awt.Color;
 
 public class Home extends JFrame {
 
 	private JPanel contentPane;
-
+	public static final int WIDTH = 1280;
+	public static final int HEIGHT = 720;
 	/**
 	 * Launch the application.
 	 */
@@ -33,10 +35,12 @@ public class Home extends JFrame {
 	 * Create the frame.
 	 */
 	public Home() {
+		setTitle("Tetris");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 600, 700);
+		setBounds(WIDTH/2, HEIGHT/2, 500, 660);
 		setResizable(false);
 		contentPane = new JPanel();
+		contentPane.setBackground(Color.BLACK);
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
@@ -45,8 +49,9 @@ public class Home extends JFrame {
 	}
 	public void NameGame(){
 		JLabel lblTetris = new JLabel("TETRIS");
-		lblTetris.setFont(new Font("Snap ITC", Font.PLAIN, 64));
-		lblTetris.setBounds(151, 16, 317, 83);
+		lblTetris.setForeground(new Color(255, 255, 255));
+		lblTetris.setFont(new Font("Bookman Old Style", Font.BOLD, 60));
+		lblTetris.setBounds((500-240)/2, 10, 240,83);
 		contentPane.add(lblTetris);
 	}
 }
